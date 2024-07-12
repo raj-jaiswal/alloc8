0a21004bf69be7a3a11c8abbf565700a69817ca6
HemantKumar01 <ytbhemant@gmail.com>
2024-07-12 13:00:13 +0530

    router setup

---
+++ client/src/pages/Others/index.jsx
@@ -0,0 +1,6 @@
+// Room allocation for everyone except freshers
+const OthersRoomAllocPage = () => {
+  return <>Works</>;
+};
+
+export default OthersRoomAllocPage;
---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Home = React.lazy(() => import("./Home"));
+const Others = React.lazy(() => import("./Others"));
+
+export { Home, Others };
