7e564c972f9a67ced6089aefb765bf15cc6a0205
HemantKumar01 <ytbhemant@gmail.com>
2024-07-14 16:24:43 +0530

    added all data

--- client/src/pages/Others/index.jsx
+++ client/src/pages/Others/index.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */ // TODO: upgrade to latest eslint tooling
 // Room allocation for everyone except freshers
 
+import "./index.css";
 import Header from "@/components/Header";
 import Stepper from "react-stepper-horizontal";
 import { Input } from "@/components/ui/input";
@@ -18,13 +19,13 @@ import {
 } from "@/components/ui/select";
 
 import { getRollNumber } from "@/lib/auth_utility";
-import { useState } from "react";
-import hostel_data from "@/data/hostel.json";
+import { useEffect, useState } from "react";
+import hostel_data from "@/data/available_rooms.json";
 
 const steps = [
   { title: "Details" },
   { title: "Hostel" },
-  { title: "Floor" },
+  // { title: "Floor" },
   { title: "Room" },
 ];
 
@@ -41,7 +42,42 @@ const ControlButtons = ({ activeStep, onPrev }) => {
         Previous
       </Button>
 
-      <Button type="submit">Next</Button>
+      <Button type="submit">
+        {activeStep == steps.length - 1 ? "Book Room" : "Next"}
+      </Button>
+    </div>
+  );
+};
+
+const StepperForm = ({
+  children,
+  style,
+  name,
+  onSubmit,
+  onPrev,
+  activeStep,
+}) => {
+  return (
+    <div style={style}>
+      <form
+        name={name}
+        className="flexbox max-w-[700px] m-auto my-10 bg-slate-100 p-5 sm:p-20 rounded-xl"
+        style={{
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          flexDirection: "column",
+        }}
+        onSubmit={(e) => {
+          onSubmit(e);
+        }}
+      >
+        {children}
+        <hr />
+        <ControlButtons
+          onPrev={onPrev}
+          activeStep={activeStep}
+        ></ControlButtons>
+      </form>
     </div>
   );
 };
@@ -63,108 +99,80 @@ const Details = ({ onNext, onPrev, activeStep }) => {
     onNext(details);
   };
   return (
-    <>
-      <form
-        name="details"
-        className="flexbox max-w-[700px] m-auto my-10 bg-slate-100 p-5 sm:p-20 rounded-xl"
-        style={{
-          justifyContent: "flex-start",
-          alignItems: "flex-start",
-          flexDirection: "column",
-        }}
-        onSubmit={(e) => {
-          handleSubmit(e);
-        }}
-      >
-        <Label htmlFor="rollno" className="w-full">
-          Roll No.
-        </Label>
-        <Input
-          type="text"
-          id="rollno"
-          name="rollno"
-          value={getRollNumber()}
-          disabled={true}
-        />
-        <Label htmlFor="gender" className="mt-3">
-          Gender
-        </Label>
-        <RadioGroup id="gender" name="gender" defaultValue="Male">
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="Male" id="male" />
-            <Label htmlFor="male">Male</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="Female" id="female" />
-            <Label htmlFor="female">Female</Label>
-          </div>
-        </RadioGroup>
-        <Label htmlFor="gender" className="mt-3">
-          Batch
-        </Label>
-        <Select id="batch" name="batch">
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Select Batch" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>B.Tech</SelectLabel>
-              <SelectItem value="BTech21">BTech'21</SelectItem>
-              <SelectItem value="BTech22">BTech'22</SelectItem>
-              <SelectItem value="BTech23">BTech'23</SelectItem>
-            </SelectGroup>
-            <SelectGroup>
-              <SelectLabel>M.Tech</SelectLabel>
-              <SelectItem value="MTech23">MTech'23</SelectItem>
-            </SelectGroup>
-            <SelectGroup>
-              <SelectLabel>M.Sc</SelectLabel>
-              <SelectItem value="MSc23">MSc'23</SelectItem>
-            </SelectGroup>
+    <StepperForm
+      name="detailData"
+      onSubmit={handleSubmit}
+      onPrev={onPrev}
+      activeStep={activeStep}
+    >
+      <Label htmlFor="rollno" className="w-full">
+        Roll No.
+      </Label>
+      <Input
+        type="text"
+        id="rollno"
+        name="rollno"
+        value={getRollNumber()}
+        disabled={true}
+      />
+      <Label htmlFor="gender" className="mt-3">
+        Gender
+      </Label>
+      <RadioGroup id="gender" name="gender" defaultValue="Male">
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="Male" id="male" />
+          <Label htmlFor="male">Male</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="Female" id="female" />
+          <Label htmlFor="female">Female</Label>
+        </div>
+      </RadioGroup>
+      <Label htmlFor="gender" className="mt-3">
+        Batch
+      </Label>
+      <Select id="batch" name="batch">
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select Batch" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>B.Tech</SelectLabel>
+            <SelectItem value="BTech21">{"BTech'21"}</SelectItem>
+            <SelectItem value="BTech22">{"BTech'22"}</SelectItem>
+            <SelectItem value="BTech23">{"BTech'23"}</SelectItem>
+          </SelectGroup>
+          <SelectGroup>
+            <SelectLabel>M.Tech</SelectLabel>
+            <SelectItem value="MTech23">{"MTech'23"}</SelectItem>
+          </SelectGroup>
+          <SelectGroup>
+            <SelectLabel>M.Sc</SelectLabel>
+            <SelectItem value="MSc23">{"MSc'23"}</SelectItem>
+          </SelectGroup>
 
-            <SelectGroup>
-              <SelectLabel>Ph.D</SelectLabel>
-              <SelectItem value="PhD14">PhD'14</SelectItem>
-              <SelectItem value="PhD16">PhD'16</SelectItem>
-              <SelectItem value="PhD17">PhD'17</SelectItem>
-              <SelectItem value="PhD18">PhD'18</SelectItem>
-              <SelectItem value="PhD19">PhD'19</SelectItem>
-              <SelectItem value="PhD20">PhD'20</SelectItem>
-              <SelectItem value="PhD21">PhD'21</SelectItem>
-              <SelectItem value="PhD22">PhD'22</SelectItem>
-              <SelectItem value="PhD23">PhD'23</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <hr />
-        <ControlButtons
-          onPrev={onPrev}
-          activeStep={activeStep}
-        ></ControlButtons>
-      </form>
-    </>
+          <SelectGroup>
+            <SelectLabel>Ph.D</SelectLabel>
+            <SelectItem value="PhD14">{"PhD'14"}</SelectItem>
+            <SelectItem value="PhD16">{"PhD'16"}</SelectItem>
+            <SelectItem value="PhD17">{"PhD'17"}</SelectItem>
+            <SelectItem value="PhD18">{"PhD'18"}</SelectItem>
+            <SelectItem value="PhD19">{"PhD'19"}</SelectItem>
+            <SelectItem value="PhD20">{"PhD'20"}</SelectItem>
+            <SelectItem value="PhD21">{"PhD'21"}</SelectItem>
+            <SelectItem value="PhD22">{"PhD'22"}</SelectItem>
+            <SelectItem value="PhD23">{"PhD'23"}</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </StepperForm>
   );
 };
-const Hostel = ({ details, onNext, onPrev, activeStep }) => {
-  const batchDetails = JSON.parse(hostel_data);
+const Hostel = ({ data, onNext, onPrev, activeStep }) => {
+  const batchDetails = {};
   console.log(batchDetails);
-  console.log(details);
-
-  let hostel_str = "";
-  for (let i = 0;i < batchDetails.length;i++) {
-    let batchDetail = batchDetails[i];
-    if (
-      batchDetail.batch.trim().toLowerCase() ===
-      details.batch.trim().toLowerCase() &&
-      batchDetail.gender.trim().toLowerCase() ===
-      details.gender.trim().toLowerCase()
-    ) {
-      hostel_str = batchDetail.hostel;
-      break;
-    }
-  }
-  const available_hostels = hostel_str.split(",");
-  console.log(available_hostels);
+  console.log(data);
+  const available_hostels = Object.keys(data);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -178,147 +186,177 @@ const Hostel = ({ details, onNext, onPrev, activeStep }) => {
     onNext(hostel);
   };
   return (
-    <>
-      <form
-        name="details"
-        className="flexbox max-w-[700px] m-auto my-10 bg-slate-100 p-5 sm:p-20 rounded-xl"
-        style={{
-          justifyContent: "flex-start",
-          alignItems: "flex-start",
-          flexDirection: "column",
-        }}
-        onSubmit={(e) => {
-          handleSubmit(e);
-        }}
+    <StepperForm
+      name="hostelData"
+      onSubmit={handleSubmit}
+      onPrev={onPrev}
+      activeStep={activeStep}
+    >
+      <Label htmlFor="hostel" className="mt-3">
+        Choose Hostel
+      </Label>
+      <RadioGroup
+        id="hostel"
+        name="hostel"
+        className="flex w-full flex-wrap sm:flex-nowrap "
       >
-        <Label htmlFor="hostel" className="mt-3">
-          Choose Hostel
+        {available_hostels.map((hostel) => {
+          return (
+            <div key={hostel} className="w-full ">
+              <Label htmlFor={hostel} className="text-lg">
+                <div className="flex flex-wrap items-center space-x-2 px-10 py-16 bg-slate-200 rounded-lg ">
+                  <RadioGroupItem value={hostel} id={hostel} />
+
+                  <span
+                    className="block ml-5"
+                    style={{ textTransform: "capitalize" }}
+                  >
+                    {hostel}
+                  </span>
+                </div>
+              </Label>
+            </div>
+          );
+        })}
+      </RadioGroup>
+    </StepperForm>
+  );
+};
+// const Floor = ({ onNext, onPrev, activeStep, data, hostel }) => {
+//   console.log(data);
+
+//   const available_floors = Object.keys(data);
+//   available_floors.sort();
+//   available_floors.reverse();
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     const form = e.target;
+//     const data = new FormData(form);
+//     let floor = data.get("floor");
+//     if (!floor) {
+//       alert("Please select a floor");
+//       return;
+//     }
+//     if (floor == "Ground") {
+//       floor = 0;
+//     }
+//     onNext(floor);
+//   };
+//   return (
+//     <StepperForm
+//       name="floorData"
+//       onSubmit={handleSubmit}
+//       onPrev={onPrev}
+//       activeStep={activeStep}
+//     >
+//       <b className="text-xl" style={{ textTransform: "capitalize" }}>
+//         {hostel}
+//       </b>
+//       <Label htmlFor="floor" className="mt-3">
+//         Choose Floor
+//       </Label>
+//       <RadioGroup id="floor" name="floor" className="flex w-full flex-wrap">
+//         {available_floors.map((floor) => {
+//           return (
+//             <div key={floor} className="w-full">
+//               <Label htmlFor={floor} className="text-lg">
+//                 <div className="flex flex-wrap items-center space-x-2 p-5  bg-slate-200 rounded-lg ">
+//                   <RadioGroupItem value={floor} id={floor} />
+
+//                   <span className="block ml-5">
+//                     {floor == 0 ? "Ground" : floor}
+//                   </span>
+//                 </div>
+//               </Label>
+//             </div>
+//           );
+//         })}
+//       </RadioGroup>
+//     </StepperForm>
+//   );
+// };
+const FloorAndRoom = ({ onNext, onPrev, activeStep, data, hostel }) => {
+  console.log(hostel, data);
+  const floors = Object.keys(data);
+  const [floor, setFloor] = useState(0);
+  const [rooms, setRooms] = useState([]);
+
+  const getRoomStatus = () => {
+    const options = ["Full", "Partial", "Available"];
+    return options[Math.random() > 0.4 ? 2 : Math.floor(Math.random() * 3)];
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const data = new FormData(form);
+  };
+
+  return (
+    <>
+      <StepperForm
+        name="roomData"
+        onSubmit={handleSubmit}
+        onPrev={onPrev}
+        activeStep={activeStep}
+      >
+        <b className="text-xl" style={{ textTransform: "capitalize" }}>
+          {hostel}
+        </b>
+        <Label htmlFor="floor" className="px-1">
+          Floor
         </Label>
-        <RadioGroup
-          id="hostel"
-          name="hostel"
-          className="flex w-full flex-wrap sm:flex-nowrap "
+        <Select
+          id="floor"
+          name="floor"
+          onValueChange={(value) => {
+            setRooms(data[value]);
+            setFloor(value);
+          }}
         >
-          {available_hostels.map((hostel) => {
-            return (
-              <div key={hostel} className="w-full ">
-                <Label htmlFor={hostel} className="text-lg">
-                  <div className="flex flex-wrap items-center space-x-2 px-10 py-16 bg-slate-300 rounded-lg ">
-                    <RadioGroupItem value={hostel} id={hostel} />
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select Floor" />
+          </SelectTrigger>
+          <SelectContent>
+            {floors.map((floor) => {
+              return (
+                <SelectItem id={floor} value={floor} key={floor}>
+                  {floor}
+                </SelectItem>
+              );
+            })}
+          </SelectContent>
+        </Select>
 
-                    <span className="block ml-5">{hostel}</span>
-                  </div>
-                </Label>
-              </div>
+        <div className="flex flex-wrap gap-3">
+          {rooms.map((room) => {
+            return (
+              <Button
+                key={floor + room}
+                className={`${getRoomStatus()} w-[50px] bg-blue-100 text-blue-700 border-blue-500 border-[1px] hover:bg-blue-500 hover:text-blue-50`}
+              >
+                {floor + room.toString().padStart(2, "0")}
+              </Button>
             );
           })}
-        </RadioGroup>
-        <hr />
-        <ControlButtons
-          onPrev={onPrev}
-          activeStep={activeStep}
-        ></ControlButtons>
-      </form>
-    </>
-  );
-};
-const Floor = ({ onNext, onPrev, activeStep, details }) => {
-  console.log(details);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const data = new FormData(form);
-    let floor = data.get("floor");
-    if (!floor) {
-      alert("Please select a floor");
-      return;
-    }
-    if (floor == "Ground") {
-      floor = 0;
-    }
-    onNext(floor);
-  };
-  return (
-    <>
-      <form
-        name="details"
-        className="flexbox max-w-[700px] m-auto my-10 bg-slate-100 p-5 sm:p-20 rounded-xl"
-        style={{
-          justifyContent: "flex-start",
-          alignItems: "flex-start",
-          flexDirection: "column",
-        }}
-        onSubmit={(e) => {
-          handleSubmit(e);
-        }}
-      >
-        <b className="text-xl">{details.hostel}</b>
-        <Label htmlFor="floor" className="mt-3">
-          Choose Floor
-        </Label>
-        <RadioGroup id="floor" name="floor" className="flex w-full flex-wrap">
-          {["Ground", 1, 2, 3, 4, 5, 6, 7].map((floor) => {
-            return (
-              <div key={floor} className="w-full">
-                <Label htmlFor={floor} className="text-lg">
-                  <div className="flex flex-wrap items-center space-x-2 p-5  bg-slate-300 rounded-lg ">
-                    <RadioGroupItem value={floor} id={floor} />
-
-                    <span className="block ml-5">{floor}</span>
-                  </div>
-                </Label>
-              </div>
-            );
-          })}
-        </RadioGroup>
-        <hr />
-        <ControlButtons
-          onPrev={onPrev}
-          activeStep={activeStep}
-        ></ControlButtons>
-      </form>
-    </>
-  );
-};
-const Room = ({ onNext, onPrev, activeStep, details }) => {
-  console.log(details);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const data = new FormData(form);
-    const details = {
-      rollno: getRollNumber(),
-      gender: data.get("gender"),
-      batch: data.get("batch"),
-    };
-    onNext(details);
-  };
-  return (
-    <>
-      <form
-        name="details"
-        className="flexbox max-w-[700px] m-auto my-10 bg-slate-100 p-5 sm:p-20 rounded-xl"
-        style={{
-          justifyContent: "flex-start",
-          alignItems: "flex-start",
-          flexDirection: "column",
-        }}
-        onSubmit={(e) => {
-          handleSubmit(e);
-        }}
-      >
-        <hr />
-        <ControlButtons
-          onPrev={onPrev}
-          activeStep={activeStep}
-        ></ControlButtons>
-      </form>
+        </div>
+      </StepperForm>
     </>
   );
 };
 const OthersRoomAllocPage = () => {
-  const [details, setDetails] = useState({});
+  const [available_rooms, setAvailableRooms] = useState({});
   let [activeStep, setActiveStep] = useState(0);
+  const [hostel, setHostel] = useState("");
+  // const [floor, setFloor] = useState(0);
+
+  const fetchAvailableRooms = (details) => {
+    console.log(hostel_data);
+    let floors =
+      hostel_data[details.batch.toLowerCase()][details.gender.toLowerCase()];
+    setAvailableRooms(floors);
+    return floors;
+  };
 
   const getComponent = () => {
     switch (activeStep) {
@@ -330,7 +368,7 @@ const OthersRoomAllocPage = () => {
               setActiveStep((a) => a - 1);
             }}
             onNext={(data) => {
-              setDetails(data);
+              fetchAvailableRooms(data);
               setActiveStep((a) => a + 1);
             }}
           />
@@ -338,48 +376,43 @@ const OthersRoomAllocPage = () => {
       case 1:
         return (
           <Hostel
-            details={details}
+            data={available_rooms}
             activeStep={1}
             onPrev={() => {
               setActiveStep((a) => a - 1);
             }}
             onNext={(data) => {
-              setDetails((detail) => {
-                detail.hostel = data;
-                return detail;
-              });
+              setHostel(data);
               setActiveStep((a) => a + 1);
             }}
           />
         );
+      // case 2:
+      //   return (
+      //     <Floor
+      //       activeStep={2}
+      //       data={available_rooms[hostel.toLowerCase()]}
+      //       hostel={hostel}
+      //       onPrev={() => {
+      //         setActiveStep((a) => a - 1);
+      //       }}
+      //       onNext={(data) => {
+      //         setFloor(data);
+      //         setActiveStep((a) => a + 1);
+      //       }}
+      //     />
+      //   );
       case 2:
         return (
-          <Floor
-            activeStep={2}
-            details={details}
-            onPrev={() => {
-              setActiveStep((a) => a - 1);
-            }}
-            onNext={(data) => {
-              setDetails((detail) => {
-                detail.floor = data;
-                return detail;
-              });
-              setActiveStep((a) => a + 1);
-            }}
-          />
-        );
-      case 3:
-        return (
-          <Room
+          <FloorAndRoom
             activeStep={3}
-            details={details}
+            data={available_rooms[hostel.toLowerCase()]}
+            hostel={hostel}
             onPrev={() => {
               setActiveStep((a) => a - 1);
             }}
             onNext={(data) => {
-              setDetails(data);
-              setActiveStep((a) => a + 1);
+              console.log(hostel, floor, data);
             }}
           />
         );
@@ -388,7 +421,7 @@ const OthersRoomAllocPage = () => {
     }
   };
   return (
-    <div>
+    <div className="bg-white h-full w-full">
       <Header></Header>
       <div style={{ fontFamily: "sans-serif" }}>
         <Stepper
