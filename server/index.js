a2edcece8c0cc22dfda5099e5ca0a5c4c592b33e
HemantKumar01 <ytbhemant@gmail.com>
2024-07-15 12:49:28 +0530

    integrated rooms

--- server/index.js
+++ server/index.js
@@ -28,8 +28,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/fresher", fresherRoutes);
-app.use("/nonfresher", nonfresherRoutes);
+app.use("/api/fresher", fresherRoutes);
+app.use("/api/nonfresher", nonfresherRoutes);
 const PORT = 8800;
 app.listen(PORT, () => {
   console.log("Connected to Backend on Port", PORT);
