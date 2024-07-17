0e63a509403081206fe82df8e223ad0693d3d65b
HemantKumar01 <ytbhemant@gmail.com>
2024-07-17 19:21:26 +0530

    revert to jspdf

--- client/src/pages/Success/index.jsx
+++ client/src/pages/Success/index.jsx
@@ -4,8 +4,8 @@ import Header from "@/components/Header";
 import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router";
 import Footer from "@/components/Footer";
-// import jsPDF from "jspdf";
-// import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 import { useMsal } from "@azure/msal-react";
 
 const SuccessPage = () => {
@@ -46,17 +46,17 @@ const SuccessPage = () => {
   }, []);
 
   const downloadPDF = () => {
-    window.print();
-    // const input = contentRef.current;
-    // html2canvas(input).then((canvas) => {
-    //   const imgData = canvas.toDataURL("image/png");
-    //   const pdf = new jsPDF("p", "mm", "a4");
-    //   const imgProps = pdf.getImageProperties(imgData);
-    //   const pdfWidth = pdf.internal.pageSize.getWidth();
-    //   const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-    //   pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
-    //   pdf.save("room_allocation_details.pdf");
-    // });
+    // window.print();
+    const input = contentRef.current;
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4");
+      const imgProps = pdf.getImageProperties(imgData);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+      pdf.save("room_allocation_details.pdf");
+    });
   };
 
   return (
