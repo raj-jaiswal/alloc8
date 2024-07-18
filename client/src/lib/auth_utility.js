d88f961eaf70e88f2cd612a13738a4131a4b2b03
HemantKumar <71439585+HemantKumar01@users.noreply.github.com>
2024-07-18 10:52:56 +0530

    phd starts with 1 for roll number

--- client/src/lib/auth_utility.js
+++ client/src/lib/auth_utility.js
@@ -5,18 +5,30 @@ function extractRollNumber(email) {
   return match ? match[0] : null;
 }
 
+import { msalInstance } from "../main.jsx";
+function getBatch(rollnum) {
+  const year = rollnum[0] + rollnum[1];
+  if (rollnum[2] == "0") return "btech" + year;
+  if (rollnum[2] == "1" && rollnum[3] == "1") return "mtech" + year;
+  if (rollnum[2] == "1" && rollnum[3] == "2") return "msc" + year;
+  if (rollnum[2] == "2") return "phd" + year;
+
+  return "error";
+}
+
 function getRollNumber() {
   let emailId = msalInstance.getAllAccounts()[0].username;
-  let rollNumber = extractRollNumber(emailId);
-  if(rollNumber==null)
-  {
-  return "Error encountered, please report this to any of the Technical Secretaries";
-  }
-  else
-  {
+  const mailParts = emailId.split("@")[0].split("_");
+  const rollNumber =
+    mailParts[0].startsWith("2") || mailParts[0].startsWith("1")
+      ? mailParts[0]
+      : mailParts[1];
+  const batch = getBatch(rollNumber);
+  if (rollNumber == null) {
+    return "Error encountered, please report this to any of the Technical Secretaries";
+  } else {
     return rollNumber;
   }
-  console.error(parts);
 }
 function getName() {
   return msalInstance.getAllAccounts()[0].name;
