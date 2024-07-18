6b3142457078bfc8daf8902f495f78b6b931c9ab
HemantKumar01 <ytbhemant@gmail.com>
2024-07-18 13:26:40 +0530

    phd bug fix

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -6,14 +6,14 @@ async function main() {}
 
 main()
   .then(async () => {
-    const data = new Set([]);
-    const ids = [];
+    const data = new Set();
     for (let batch in available_rooms) {
       for (let gender in available_rooms[batch]) {
         for (let hostel in available_rooms[batch][gender]) {
           console.log("adding for", batch, gender, hostel);
           for (let floor in available_rooms[batch][gender][hostel]) {
             for (let room of available_rooms[batch][gender][hostel][floor]) {
+              let hardcodedBatch = null;
               let capacity = 2;
               if (hostel.startsWith("asima") && batch == "btech23") {
                 capacity = 3;
@@ -35,7 +35,7 @@ main()
                 (batch == "phd21" && gender == "male")
               ) {
                 capacity = 1;
-                batch = "phd14to20male";
+                hardcodedBatch = "phd14to20male";
               }
 
               if (
@@ -51,7 +51,7 @@ main()
                 gender == "female"
               ) {
                 capacity = 1;
-                batch = "phd17to23female";
+                hardcodedBatch = "phd17to23female";
               }
 
               //   console.log(
@@ -64,43 +64,61 @@ main()
               //     floor.toString() + room.toString().padStart(2, "0")
               //   }-${batch}`
               // );
-
-              data.add({
-                roomId: `${hostel}-${
-                  floor.toString() + room.toString().padStart(2, "0")
-                }-${batch}`,
-                hostel,
-                gender,
-                floor,
-                roomNum: room.toString().padStart(3, "0"),
-                batch,
-                capacity,
-                numFilled: 0,
-                students: [],
-                roommateCode: "",
-              });
+              console.log(
+                `${hostel}-${room.toString().padStart(3, "0")}-${
+                  hardcodedBatch || batch
+                }`
+              );
+              data.add(
+                JSON.stringify({
+                  roomId: `${hostel}-${room.toString().padStart(3, "0")}-${
+                    hardcodedBatch || batch
+                  }`,
+                  hostel,
+                  gender,
+                  floor,
+                  roomNum: room.toString().padStart(3, "0"),
+                  batch: hardcodedBatch || batch,
+                  capacity,
+                  numFilled: 0,
+                  students: [],
+                  roommateCode: "",
+                })
+              );
             }
           }
         }
       }
     }
 
-    // const findDuplicates = (arr) => {
-    //   let sorted_arr = arr.slice().sort(); // You can define the comparing function here.
-    //   // JS by default uses a crappy string compare.
-    //   // (we use slice to clone the array so the
-    //   // original array won't be modified)
-    //   let results = [];
-    //   for (let i = 0; i < sorted_arr.length - 1; i++) {
-    //     if (sorted_arr[i + 1] == sorted_arr[i]) {
-    //       results.push(sorted_arr[i]);
-    //     }
-    //   }
-    //   return results;
-    // };
-    // console.log("duplicate IDs", findDuplicates(ids));
+    const findDuplicates = (arr) => {
+      let sorted_arr = arr.slice().sort(); // You can define the comparing function here.
+      // JS by default uses a crappy string compare.
+      // (we use slice to clone the array so the
+      // original array won't be modified)
+      let results = [];
+      for (let i = 0; i < sorted_arr.length - 1; i++) {
+        if (sorted_arr[i + 1] == sorted_arr[i]) {
+          results.push(sorted_arr[i]);
+        }
+      }
+      return results;
+    };
+    const finalData = [];
+    const ids = [];
+    for (const x of data) {
+      console.log(JSON.parse(x).roomId);
+      ids.push(JSON.parse(x).roomId);
+      finalData.push(JSON.parse(x));
+    }
+    console.log(
+      "duplicate IDs",
+      findDuplicates(ids),
+      ", ids length: ",
+      ids.length
+    );
     const createMany = await prisma.rooms.createMany({
-      data: data,
+      data: finalData,
     });
   })
   .catch(async (e) => {
